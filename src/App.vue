8994fca99b9aacd0f8e5be4b9a9e8b732105857a
skylake8800 <422319368@qq.com>
2025-10-22 20:48:34 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import Index from './components/Index.vue'
+</script>
+
+<template>
+  <div class="background">
+
+  <Index msg="Vite + 1Vue" />
+  </div>
+</template>
+
+<style scoped>
+.background {
+
+
+
+}
+</style>
---
+++ src/components/Index.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="main background">
+    <div class="left"></div>
+    <div class="center">
+      <MyDatatime></MyDatatime>
+    </div>
+    <div class="right"></div>
+  </div>
+</template>
+
+<style scoped>
+.main{
+  display: flex;
+  flex-direction: row;
+}
+.background {
+  background-image: url("../assets/catalina.jpg");
+  background-size: 100% 100%;
+  position: fixed;
+  top: 0px;
+  width: 100%;
+  height: 100%;
+}
+.left{
+  width: 25%;
+  border: 1px solid red;
+  height: 100%;
+}
+.center{
+  width: 50%;
+  border: 1px solid blue;
+  height: 100%;
+}
+.right{
+  width: 25%;
+  border: 1px solid green;
+  height: 100%;
+}
+</style>
